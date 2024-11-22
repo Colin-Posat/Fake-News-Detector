104c8c3c150e9398b03acad5d1969ebc9c5fc3a9
ColinPosat <posc4705@lgsstudent.org>
2024-11-21 18:22:12 -0800

    req final

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 import re
 import string 
 import joblib
 from dotenv import load_dotenv
 import os
 import openai
+import modal
 load_dotenv()
 from flask import Flask, render_template, request
 
